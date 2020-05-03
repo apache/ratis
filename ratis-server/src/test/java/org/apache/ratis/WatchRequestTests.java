3d9c571f0a3eb34b8f8b35a1b3dfc60efc3ad57b
runzhiwang <runzhiwang@tencent.com>
2020-05-03 15:08:50 +0800

    no retry to avoid NotLeaderException

--- ratis-server/src/test/java/org/apache/ratis/WatchRequestTests.java
+++ ratis-server/src/test/java/org/apache/ratis/WatchRequestTests.java
@@ -23,12 +23,12 @@ import org.apache.ratis.client.RaftClientConfigKeys;
 import org.apache.ratis.conf.RaftProperties;
 import org.apache.ratis.proto.RaftProtos.CommitInfoProto;
 import org.apache.ratis.proto.RaftProtos.ReplicationLevel;
-import org.apache.ratis.protocol.NotLeaderException;
+import org.apache.ratis.protocol.AlreadyClosedException;
 import org.apache.ratis.protocol.NotReplicatedException;
 import org.apache.ratis.protocol.RaftClientReply;
-import org.apache.ratis.protocol.RaftRetryFailureException;
 import org.apache.ratis.protocol.TimeoutIOException;
 import org.apache.ratis.retry.RetryPolicies;
+import org.apache.ratis.retry.RetryPolicy;
 import org.apache.ratis.server.RaftServerConfigKeys;
 import org.apache.ratis.server.impl.RaftServerImpl;
 import org.apache.ratis.server.impl.RaftServerTestUtil;
@@ -114,13 +114,11 @@ public abstract class WatchRequestTests<CLUSTER extends MiniRaftCluster>
       }
     }
 
-    CompletableFuture<RaftClientReply> sendWatchRequest(long logIndex)
+    CompletableFuture<RaftClientReply> sendWatchRequest(long logIndex, RetryPolicy policy)
         throws Exception {
 
       try (final RaftClient watchClient =
-          cluster.createClient(RaftTestUtil.waitForLeader(cluster).getId(),
-          RetryPolicies.retryUpToMaximumCountWithFixedSleep(5,
-          TimeDuration.valueOf(5, TimeUnit.SECONDS)))) {
+          cluster.createClient(RaftTestUtil.waitForLeader(cluster).getId(), policy)) {
 
         CompletableFuture<RaftClientReply> reply =
             watchClient.sendAsync(new RaftTestUtil.SimpleMessage("message"));
@@ -448,25 +446,22 @@ public abstract class WatchRequestTests<CLUSTER extends MiniRaftCluster>
 
     CompletableFuture<RaftClientReply> watchReply;
     // watch 1000 which will never be committed
-    // so client can not receive reply, and connection closed, throw TimeoutException
-    watchReply = p.sendWatchRequest(1000);
+    // so client can not receive reply, and connection closed, throw TimeoutException.
+    // We should not retry, because if retry, RaftClientImpl::handleIOException will random select a leader,
+    // then sometimes throw NotLeaderException.
+    watchReply = p.sendWatchRequest(1000, RetryPolicies.noRetry());
 
     try {
       watchReply.get();
       fail("runTestWatchRequestClientTimeout failed");
     } catch (Exception ex) {
       LOG.error("error occurred", ex);
-      Assert.assertEquals(RaftRetryFailureException.class,
+      Assert.assertEquals(AlreadyClosedException.class,
           ex.getCause().getClass());
       if (ex.getCause() != null) {
         if (ex.getCause().getCause() != null) {
-          // when client closed and throw TimeoutException,
-          // RaftClientImpl::handleIOException will random select a leader
-          // because suggested new leader is null.
-          // So it maybe sometimes throw NotLeaderException.
-          Assert.assertTrue(
-              ex.getCause().getCause().getClass() == TimeoutIOException.class ||
-              ex.getCause().getCause().getClass() == NotLeaderException.class);
+          Assert.assertEquals(TimeoutIOException.class,
+              ex.getCause().getCause().getClass());
         }
       }
     }
