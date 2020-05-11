4c9e8e7b63d64f6e2378fc5fd0e84267315f28de
Mukul Kumar Singh <mukul@cloudera.com>
2020-05-11 22:31:41 +0530

    RATIS-936. Fix ratis-hadoop with non shaded protobuf dependency.

--- ratis-hadoop/src/main/java/org/apache/ratis/hadooprpc/server/RaftServerProtocolServerSideTranslatorPB.java
+++ ratis-hadoop/src/main/java/org/apache/ratis/hadooprpc/server/RaftServerProtocolServerSideTranslatorPB.java
@@ -18,17 +18,22 @@
 package org.apache.ratis.hadooprpc.server;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import org.apache.hadoop.classification.InterfaceAudience;
+import org.apache.ratis.proto.hadoop.HadoopCompatibilityProtos.ServerOps;
+import org.apache.ratis.proto.hadoop.HadoopCompatibilityProtos.ServerReplyProto;
+import org.apache.ratis.proto.hadoop.HadoopCompatibilityProtos.ServerRequestProto;
 import org.apache.ratis.server.protocol.RaftServerProtocol;
-import org.apache.ratis.thirdparty.com.google.protobuf.RpcController;
-import org.apache.ratis.thirdparty.com.google.protobuf.ServiceException;
+import com.google.protobuf.RpcController;
+import com.google.protobuf.ServiceException;
 import org.apache.ratis.proto.RaftProtos.AppendEntriesReplyProto;
 import org.apache.ratis.proto.RaftProtos.AppendEntriesRequestProto;
 import org.apache.ratis.proto.RaftProtos.InstallSnapshotReplyProto;
 import org.apache.ratis.proto.RaftProtos.InstallSnapshotRequestProto;
 import org.apache.ratis.proto.RaftProtos.RequestVoteReplyProto;
 import org.apache.ratis.proto.RaftProtos.RequestVoteRequestProto;
+import org.apache.ratis.thirdparty.com.google.protobuf.GeneratedMessageV3;
 
 @InterfaceAudience.Private
 public class RaftServerProtocolServerSideTranslatorPB
@@ -40,34 +45,45 @@ public class RaftServerProtocolServerSideTranslatorPB
   }
 
   @Override
-  public RequestVoteReplyProto requestVote(
-      RpcController unused, RequestVoteRequestProto request)
-      throws ServiceException {
+  public ServerReplyProto sendServer(RpcController unused, ServerRequestProto requestProto) throws ServiceException {
+    ServerOps type = requestProto.getType();
+    ByteBuffer buffer = requestProto.getRequest().asReadOnlyByteBuffer();
+    GeneratedMessageV3 respone = null;
     try {
-      return impl.requestVote(request);
+      switch (type) {
+        case requestVote:
+          respone = requestVote(RequestVoteRequestProto.parseFrom(buffer));
+          break;
+        case installSnapshot:
+          respone = installSnapshot(InstallSnapshotRequestProto.parseFrom(buffer));
+          break;
+        case appendEntries:
+          respone = appendEntries(AppendEntriesRequestProto.parseFrom(buffer));
+          break;
+        default:
+          throw new IOException("Invalid Request Type:" + type);
+      }
+      return ServerReplyProto.newBuilder()
+          .setType(type)
+          .setResponse(com.google.protobuf.ByteString.copyFrom(respone.toByteArray()))
+          .build();
     } catch(IOException ioe) {
       throw new ServiceException(ioe);
     }
   }
 
-  @Override
-  public AppendEntriesReplyProto appendEntries(
-      RpcController unused, AppendEntriesRequestProto request)
-      throws ServiceException {
-    try {
-      return impl.appendEntries(request);
-    } catch(IOException ioe) {
-      throw new ServiceException(ioe);
-    }
+  public RequestVoteReplyProto requestVote(RequestVoteRequestProto request)
+      throws IOException {
+    return impl.requestVote(request);
   }
 
-  @Override
-  public InstallSnapshotReplyProto installSnapshot(RpcController controller,
-      InstallSnapshotRequestProto request) throws ServiceException {
-    try {
-      return impl.installSnapshot(request);
-    } catch(IOException ioe) {
-      throw new ServiceException(ioe);
-    }
+  public AppendEntriesReplyProto appendEntries(AppendEntriesRequestProto request)
+      throws IOException {
+    return impl.appendEntries(request);
+  }
+
+  public InstallSnapshotReplyProto installSnapshot(
+      InstallSnapshotRequestProto request) throws IOException {
+    return impl.installSnapshot(request);
   }
 }
