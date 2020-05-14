26cfdb7efcb2da7e0e0ce56a20f9fbd8e1e1f175
isa <esa.hekmat@gmail.com>
2020-05-14 14:52:38 +0430

    add license header to new files and correct some minor checkstyle issues

--- ratis-examples/src/test/java/org/apache/ratis/examples/counter/TestCounter.java
+++ ratis-examples/src/test/java/org/apache/ratis/examples/counter/TestCounter.java
@@ -1,16 +1,36 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ratis.examples.counter;
 
-import org.apache.ratis.*;
-import org.apache.ratis.client.*;
-import org.apache.ratis.examples.*;
-import org.apache.ratis.examples.counter.server.*;
-import org.apache.ratis.protocol.*;
-import org.junit.*;
-import org.junit.runners.*;
+import org.apache.ratis.MiniRaftCluster;
+import org.apache.ratis.client.RaftClient;
+import org.apache.ratis.examples.ParameterizedBaseTest;
+import org.apache.ratis.examples.counter.server.CounterStateMachine;
+import org.apache.ratis.protocol.Message;
+import org.apache.ratis.protocol.RaftClientReply;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
 
-import java.io.*;
-import java.nio.charset.*;
-import java.util.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Collection;
 
 public class TestCounter extends ParameterizedBaseTest {
 
