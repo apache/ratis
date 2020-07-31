163fecb7ac25f423d017d31d143ea1112513720d
Ansh Khanna <ansh.khanna@cloudera.com>
2020-07-31 16:01:37 -0400

    increasing check time for appendentries

--- ratis-test/src/test/java/org/apache/ratis/grpc/TestGrpcMessageMetrics.java
+++ ratis-test/src/test/java/org/apache/ratis/grpc/TestGrpcMessageMetrics.java
@@ -24,12 +24,14 @@ import org.apache.ratis.client.RaftClient;
 import org.apache.ratis.grpc.server.GrpcService;
 import org.apache.ratis.metrics.JVMMetrics;
 import org.apache.ratis.metrics.RatisMetricRegistry;
+import org.apache.ratis.protocol.RaftClientReply;
 import org.apache.ratis.server.impl.RaftServerImpl;
 import org.apache.ratis.util.JavaUtils;
 import org.apache.ratis.util.TimeDuration;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static org.apache.ratis.RaftTestUtil.waitForLeader;
@@ -51,13 +53,11 @@ public class TestGrpcMessageMetrics extends BaseTest
   }
 
   static void sendMessages(MiniRaftCluster cluster) throws Exception {
-    int numMsg = 5;
-    final RaftTestUtil.SimpleMessage[] messages = RaftTestUtil.SimpleMessage.create(numMsg);
     waitForLeader(cluster);
     try (final RaftClient client = cluster.createClient()) {
-      for (RaftTestUtil.SimpleMessage message : messages) {
-        client.send(message);
-      }
+      CompletableFuture<RaftClientReply> replyFuture =  client.sendAsync(new RaftTestUtil.SimpleMessage("abc"));
+      TimeDuration.valueOf(5 , TimeUnit.SECONDS).sleep();
+      Assert.assertTrue(!replyFuture.isDone());
     }
     // Wait for commits to happen on leader
     JavaUtils.attempt(() -> assertMessageCount(cluster.getLeader()), 100, HUNDRED_MILLIS, cluster.getLeader().getId() + "-assertMessageCount", null);
