615f517fa2d94bd5c335b0ce45788c141f813405
Shashikant Banerjee <shashikant@apache.org>
2021-03-17 17:24:39 +0530

    Addressed review comments.

--- ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
@@ -526,21 +526,15 @@ class RaftServerImpl implements RaftServer.Division,
       break;
 
     case FOLLOWER:
-      if (role.getFollowerState().isPresent()) {
-        final FollowerState fs = role.getFollowerState().get();
-        final ServerRpcProto leaderInfo =
-          ServerProtoUtils.toServerRpcProto(getRaftConf().
-            getPeer(state.getLeaderId()), fs.getLastRpcTime().elapsedTimeMs());
-        roleInfo.setFollowerInfo(FollowerInfoProto.newBuilder()
-          .setLeaderInfo(leaderInfo).setOutstandingOp(fs.getOutstandingOp()));
-      } else {
-        // FollowerState can be null while adding a new peer as it is not
-        // a voting member yet
-        final ServerRpcProto leaderInfo = ServerProtoUtils.
-          toServerRpcProto(getRaftConf().getPeer(state.getLeaderId()), 0);
-        roleInfo.setFollowerInfo(FollowerInfoProto.newBuilder().
-          setLeaderInfo(leaderInfo).setOutstandingOp(0));
-      }
+      final Optional<FollowerState> fs = role.getFollowerState();
+      final ServerRpcProto leaderInfo = ServerProtoUtils.toServerRpcProto(
+        getRaftConf().getPeer(state.getLeaderId()),
+        fs.map(FollowerState::getLastRpcTime).map(Timestamp::elapsedTimeMs).orElse(0L));
+      // FollowerState can be null while adding a new peer as it is not
+      // a voting member yet
+      roleInfo.setFollowerInfo(FollowerInfoProto.newBuilder()
+        .setLeaderInfo(leaderInfo)
+        .setOutstandingOp(fs.map(FollowerState::getOutstandingOp).orElse(0)));
       break;
 
     case LEADER:
