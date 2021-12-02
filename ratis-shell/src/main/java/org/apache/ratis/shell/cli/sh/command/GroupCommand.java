a80b6e5cdf9f52f71ec7e4ce671187073b49f6ed
Yaolong Liu <lylpysz1@163.com>
2021-12-02 23:05:58 +0800

    make code better

---
+++ ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/command/GroupCommand.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ratis.shell.cli.sh.command;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.ratis.shell.cli.Command;
+import org.apache.ratis.shell.cli.sh.command.AbstractRatisCommand;
+import org.apache.ratis.shell.cli.sh.command.Context;
+import org.apache.ratis.shell.cli.sh.group.GroupInfoCommand;
+import org.apache.ratis.shell.cli.sh.group.GroupListCommand;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Command for the ratis group
+ */
+public class GroupCommand extends AbstractRatisCommand {
+
+  private static final Map<String, Function<Context, ? extends Command>>
+          SUB_COMMANDS = new HashMap<>();
+
+  static {
+    SUB_COMMANDS.put("info", GroupInfoCommand::new);
+    SUB_COMMANDS.put("list", GroupListCommand::new);
+  }
+
+  private Map<String, Command> mSubCommands = new HashMap<>();
+
+  /**
+   * @param context command context
+   */
+  public GroupCommand(Context context) {
+    super(context);
+    SUB_COMMANDS.forEach((name, constructor) -> {
+      mSubCommands.put(name, constructor.apply(context));
+    });
+  }
+
+  @Override
+  public String getCommandName() {
+    return "group";
+  }
+
+  @Override
+  public String getUsage() {
+
+    StringBuilder usage = new StringBuilder(getCommandName());
+    for (String cmd : SUB_COMMANDS.keySet()) {
+      usage.append(" [").append(cmd).append("]");
+    }
+    return usage.toString();
+  }
+
+  @Override
+  public String getDescription() {
+    return description();
+  }
+
+  @Override
+  public boolean hasSubCommand() {
+    return true;
+  }
+
+  @Override
+  public Map<String, Command> getSubCommands() {
+    return mSubCommands;
+  }
+
+  @Override
+  public Options getOptions() {
+    return super.getOptions().addOption(
+        Option.builder()
+            .option(GROUPID_OPTION_NAME)
+            .hasArg()
+            .required()
+            .desc("the group id")
+            .build());
+  }
+
+  /**
+   * @return command's description
+   */
+  public static String description() {
+    return "Manage the ratis gropu, See sub-commands' descriptions for more details.";
+  }
+}
--- ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/group/GroupListCommand.java
+++ ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/group/GroupListCommand.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.ratis.shell.cli.sh.command;
+package org.apache.ratis.shell.cli.sh.group;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
@@ -24,6 +24,8 @@ import org.apache.ratis.client.RaftClient;
 import org.apache.ratis.protocol.GroupListReply;
 import org.apache.ratis.protocol.RaftPeerId;
 import org.apache.ratis.shell.cli.RaftUtils;
+import org.apache.ratis.shell.cli.sh.command.AbstractRatisCommand;
+import org.apache.ratis.shell.cli.sh.command.Context;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -31,18 +33,19 @@ import java.net.InetSocketAddress;
 /**
  * Command for querying the group information of a ratis server.
  */
-public class GroupInfoCommand extends AbstractRatisCommand{
+public class GroupListCommand extends AbstractRatisCommand {
   public static final String SERVER_ADDRESS_OPTION_NAME = "serverAddress";
+
   /**
    * @param context command context
    */
-  public GroupInfoCommand(Context context) {
+  public GroupListCommand(Context context) {
     super(context);
   }
 
   @Override
   public String getCommandName() {
-    return "groupInfo";
+    return "list";
   }
 
   @Override
@@ -57,21 +60,23 @@ public class GroupInfoCommand extends AbstractRatisCommand{
     final InetSocketAddress serverAddress = InetSocketAddress.createUnresolved(str[0], Integer.parseInt(str[1]));
     final RaftPeerId peerId = RaftUtils.getPeerId(serverAddress);
 
-    try(final RaftClient raftClient = RaftUtils.createClient(getRaftGroup())){
+    try(final RaftClient raftClient = RaftUtils.createClient(getRaftGroup())) {
       GroupListReply reply = raftClient.getGroupManagementApi(peerId).list();
-      processReply(reply, () -> String.format("Failed to get group information of server %s",strAddr));
+      processReply(reply, () -> String.format("Failed to get group information of server %s", strAddr));
       printf(String.format("The server %s is in %d groups, and the groupIds is: %s",
-              strAddr, reply.getGroupIds().size(),reply.getGroupIds()));
+              strAddr, reply.getGroupIds().size(), reply.getGroupIds()));
     }
     return 0;
+
   }
 
   @Override
   public String getUsage() {
     return String.format("%s"
                     + " -%s <PEER0_HOST:PEER0_PORT,PEER1_HOST:PEER1_PORT,PEER2_HOST:PEER2_PORT>"
+                    + " [-%s <RAFT_GROUP_ID>]"
                     + "-%s <PEER0_HOST:PEER0_PORT>",
-            getCommandName(), PEER_OPTION_NAME, SERVER_ADDRESS_OPTION_NAME);
+            getCommandName(), PEER_OPTION_NAME, GROUPID_OPTION_NAME, SERVER_ADDRESS_OPTION_NAME);
   }
 
   @Override
@@ -81,13 +86,13 @@ public class GroupInfoCommand extends AbstractRatisCommand{
 
   @Override
   public Options getOptions() {
-    return super.getOptions().addOption(
-        Option.builder()
-            .option(SERVER_ADDRESS_OPTION_NAME)
-            .hasArg()
-            .required()
-            .desc("the server address")
-            .build());
+    return super.getOptions()
+            .addOption(Option.builder()
+                    .option(SERVER_ADDRESS_OPTION_NAME)
+                    .hasArg()
+                    .required()
+                    .desc("the server address")
+                    .build());
   }
 
   /**
