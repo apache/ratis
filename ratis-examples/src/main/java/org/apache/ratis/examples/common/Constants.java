7206975decd2f307483fafe2eb5985c22d082bdf
dragonyliu <dragonyliu@tencent.com>
2022-02-24 19:11:00 +0800

    fix checkstyle error

--- ratis-examples/src/main/java/org/apache/ratis/examples/common/Constants.java
+++ ratis-examples/src/main/java/org/apache/ratis/examples/common/Constants.java
@@ -32,7 +32,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.UUID;
