415885d049bc34d01966aee36a666c5883540ecd
Ben Horowitz <ben@woozlesaurus.com>
2022-07-12 01:30:37 -0700

    RATIS-1621. Fix intermittent failure in TestLeaderElectionMetrics
    (cherry picked from commit d674f988dfbe618aadc35e3b7e860e94670a15b1)

--- ratis-server/src/test/java/org/apache/ratis/server/metrics/TestLeaderElectionMetrics.java
+++ ratis-server/src/test/java/org/apache/ratis/server/metrics/TestLeaderElectionMetrics.java
@@ -52,7 +52,7 @@ public class TestLeaderElectionMetrics {
     leaderElectionMetrics.onNewLeaderElectionCompletion();
     Long leaderElectionLatency = (Long) ratisMetricRegistry.getGauges((s, metric) ->
         s.contains(LAST_LEADER_ELECTION_ELAPSED_TIME)).values().iterator().next().getValue();
-    assertTrue(leaderElectionLatency > 0L);
+    assertTrue(leaderElectionLatency >= 0L);
   }
 
   @Test
