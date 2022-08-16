6fc18b76080c0bc26f9bee13aa9357f2e0ca867c
dragonyliu <dragonyliu@tencent.com>
2022-08-16 21:41:13 +0800

    Revert "RATIS-1391. Update library dropwizard.metrics version to 4.x (#632)"
    This reverts commit dbb4ffa3

--- ratis-metrics/src/main/java/org/apache/ratis/metrics/MetricsReporting.java
+++ ratis-metrics/src/main/java/org/apache/ratis/metrics/MetricsReporting.java
@@ -21,7 +21,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import com.codahale.metrics.ConsoleReporter;
-import com.codahale.metrics.jmx.JmxReporter;
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.JmxReporter.Builder;
 import org.apache.ratis.util.TimeDuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,10 +53,9 @@ public final class MetricsReporting {
 
   public static Consumer<RatisMetricRegistry> jmxReporter() {
     return registry -> {
-      JmxReporter.Builder builder =
+      Builder builder =
           JmxReporter.forRegistry(registry.getDropWizardMetricRegistry());
       builder.inDomain(registry.getMetricRegistryInfo().getApplicationName());
-      builder.createsObjectNamesWith(new RatisObjectNameFactory());
       JmxReporter reporter = builder.build();
       reporter.start();
 
