0755cfc3a3b3ac80242af8b4ed19ce972a24b63c
nibiruxu <axcsd3692@qq.com>
2022-09-13 17:29:16 +0800

    RATIS-1707. Fix corner case when getPrevious in LogAppenderBase

--- ratis-server/src/main/java/org/apache/ratis/server/leader/LogAppenderBase.java
+++ ratis-server/src/main/java/org/apache/ratis/server/leader/LogAppenderBase.java
@@ -158,6 +158,11 @@ public abstract class LogAppenderBase implements LogAppender {
       }
     }
 
+    final long raftLogLastPurgedIndex = getRaftLog().getStartIndex() - 1;
+    if (raftLogLastPurgedIndex == previousIndex) {
+      return TermIndex.valueOf(server.getInfo().getCurrentTerm(), raftLogLastPurgedIndex);
+    }
+
     return null;
   }
 
