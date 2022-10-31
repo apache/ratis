2fe4d0fb17de2f01fcc14c049c738180aedafcfc
Chris Nauroth <cnauroth@apache.org>
2022-10-31 15:01:08 -0700

    RATIS-1340. Discovery of Bash source path is incompatible with CDPATH. (#772)
    (cherry picked from commit e87e16dd25865239edf8e50478539357b779519f)

--- ratis-examples/src/main/bin/start-all.sh
+++ ratis-examples/src/main/bin/start-all.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
 
 USAGE="start-all.sh <example> <subcommand>"
 
