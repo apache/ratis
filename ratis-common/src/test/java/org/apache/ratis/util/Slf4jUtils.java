43d8f220f9f3a32effef74db9d6ff1630d45f5d8
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2022-11-17 05:32:58 -0800

    RATIS-1742. Remove slf4j-log4j12 test dependency from ratis-common and ratis-server. (#781)
    (cherry picked from commit f2447cfe0b94ad5f9e72dff1623fa90b6f42f4f5)

--- ratis-common/src/test/java/org/apache/ratis/util/Slf4jUtils.java
+++ ratis-common/src/test/java/org/apache/ratis/util/Slf4jUtils.java
@@ -17,34 +17,113 @@
  */
 package org.apache.ratis.util;
 
-import org.apache.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.event.Level;
 
-import java.util.Optional;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Supplier;
 
 public interface Slf4jUtils {
-
   static void setLogLevel(Logger logger, Level level) {
     final String name = logger.getName();
-    if (LogUtils.LOG.isTraceEnabled()) {
-      LogUtils.LOG.trace("", new Throwable("Set " + name + " log level to " + level));
-    } else {
-      LogUtils.LOG.info("Set {} log level to {}", name, level);
-    }
+    final boolean set = Log4jUtils.setLevel(name, level.name());
 
-    Optional.ofNullable(LogManager.getLogger(name))
-        .ifPresent(log -> log.setLevel(getLog4jLevel(level)));
+    final String prefix = set? "Successfully": "Failed to";
+    if (LogUtils.LOG.isTraceEnabled()) {
+      LogUtils.LOG.trace("", new Throwable(prefix + " set " + name + " log level to " + level));
+    } else {
+      LogUtils.LOG.info(prefix + " set {} log level to {}", name, level);
+    }
   }
 
-  static org.apache.log4j.Level getLog4jLevel(Level level) {
-    switch (level) {
-      case ERROR: return org.apache.log4j.Level.ERROR;
-      case WARN: return org.apache.log4j.Level.WARN;
-      case INFO: return org.apache.log4j.Level.INFO;
-      case DEBUG: return org.apache.log4j.Level.DEBUG;
-      case TRACE: return org.apache.log4j.Level.TRACE;
+  interface Log4jUtils {
+    String PACKAGE_NAME = "org.apache.log4j";
+
+    /**
+     * Use reflection to invoke setLevel.
+     *
+     * @param loggerName The logger name.
+     * @param levelName The level name.
+     * @return true iff the level is successfully set.
+     */
+    static boolean setLevel(String loggerName, String levelName) {
+      final Field level = Level.valueOf(levelName);
+      if (level == null) {
+        return false;
+      }
+
+      try {
+        final Object logger = LogManager.getLogger().invoke(null, loggerName);
+        Logger.setLevel().invoke(logger, level.get(null));
+      } catch (Exception ignored) {
+        return false;
+      }
+      return true;
     }
-    throw new IllegalArgumentException("Unexpected level " + level);
+
+    interface LogManager {
+      String CLASS_NAME = PACKAGE_NAME + ".LogManager";
+      Supplier<Method> GET_LOGGER = MemoizedSupplier.valueOf(LogManager::getLoggerImpl);
+
+      static Method getLogger() {
+        return GET_LOGGER.get();
+      }
+
+      static Method getLoggerImpl() {
+        final Class<?> clazz = ReflectionUtils.getClassByNameOrNull(CLASS_NAME);
+        if (clazz == null) {
+          return null;
+        }
+        final Class<?>[] argClasses = {String.class};
+        try {
+          return clazz.getMethod("getLogger", argClasses);
+        } catch (Exception e) {
+          return null;
+        }
+      }
+    }
+
+    interface Level {
+      String CLASS_NAME = PACKAGE_NAME + ".Level";
+      ConcurrentMap<String, Field> LEVELS = new ConcurrentHashMap<>();
+
+      static Field valueOf(String level) {
+        return LEVELS.computeIfAbsent(level, Level::valueOfImpl);
+      }
+
+      static Field valueOfImpl(String level) {
+        try {
+          final Class<?> clazz = ReflectionUtils.getClassByNameOrNull(CLASS_NAME);
+          return clazz.getDeclaredField(level);
+        } catch (Exception e) {
+          return null;
+        }
+      }
+    }
+
+    interface Logger {
+      String CLASS_NAME = PACKAGE_NAME + ".Logger";
+      Supplier<Method> SET_LEVEL = MemoizedSupplier.valueOf(Logger::setLevelImpl);
+
+      static Method setLevel() {
+        return SET_LEVEL.get();
+      }
+
+      static Method setLevelImpl() {
+        final Class<?> clazz = ReflectionUtils.getClassByNameOrNull(CLASS_NAME);
+        if (clazz == null) {
+          return null;
+        }
+        final Class<?>[] argClasses = {ReflectionUtils.getClassByNameOrNull(Level.CLASS_NAME)};
+        try {
+          return clazz.getMethod("setLevel", argClasses);
+        } catch (Exception e) {
+          return null;
+        }
+      }
+    }
   }
 }
