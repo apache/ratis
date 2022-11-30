f7a6f49822622e07d67dcde31e3a0a063672557d
szywilliam <szywilliam@163.com>
2022-11-30 15:37:09 +0800

    apply review patch

--- ratis-server/src/main/java/org/apache/ratis/server/impl/ServerState.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/ServerState.java
@@ -83,7 +83,7 @@ class ServerState {
    * The server ID of the leader for this term. Null means either there is
    * no leader for this term yet or this server does not know who it is yet.
    */
-  private volatile RaftPeerId leaderId;
+  private final AtomicReference<RaftPeerId> leaderId = new AtomicReference<>();
   /**
    * Candidate that this peer granted vote for in current term (or null if none)
    */
@@ -120,7 +120,6 @@ class ServerState {
         stateMachine.getStateMachineStorage());
 
     // On start the leader is null, start the clock now
-    this.leaderId = null;
     this.lastNoLeaderTime = Timestamp.currentTime();
     this.noLeaderTimeout = RaftServerConfigKeys.Notification.noLeaderTimeout(prop);
 
@@ -207,7 +206,7 @@ class ServerState {
   }
 
   RaftPeerId getLeaderId() {
-    return leaderId;
+    return leaderId.get();
   }
 
   /**
@@ -245,7 +244,8 @@ class ServerState {
   }
 
   void setLeader(RaftPeerId newLeaderId, Object op) {
-    if (!Objects.equals(leaderId, newLeaderId)) {
+    final RaftPeerId oldLeaderId = leaderId.getAndSet(newLeaderId);
+    if (!Objects.equals(oldLeaderId, newLeaderId)) {
       String suffix;
       if (newLeaderId == null) {
         // reset the time stamp when a null leader is assigned
@@ -258,9 +258,8 @@ class ServerState {
         server.getStateMachine().event().notifyLeaderChanged(getMemberId(), newLeaderId);
       }
       LOG.info("{}: change Leader from {} to {} at term {} for {}{}",
-          getMemberId(), leaderId, newLeaderId, getCurrentTerm(), op, suffix);
-      leaderId = newLeaderId;
-      if (leaderId != null) {
+          getMemberId(), oldLeaderId, newLeaderId, getCurrentTerm(), op, suffix);
+      if (newLeaderId != null) {
         server.finishTransferLeadership();
         server.onGroupLeaderElected();
       }
@@ -323,13 +322,15 @@ class ServerState {
     final long current = currentTerm.get();
     if (leaderTerm < current) {
       return false;
-    } else if (leaderTerm > current || this.leaderId == null) {
+    }
+    final RaftPeerId curLeaderId = getLeaderId();
+    if (leaderTerm > current || curLeaderId == null) {
       // If the request indicates a term that is greater than the current term
       // or no leader has been set for the current term, make sure to update
       // leader and term later
       return true;
     }
-    return this.leaderId.equals(peerLeaderId);
+    return curLeaderId.equals(peerLeaderId);
   }
 
   static int compareLog(TermIndex lastEntry, TermIndex candidateLastEntry) {
@@ -353,7 +354,7 @@ class ServerState {
 
   @Override
   public String toString() {
-    return getMemberId() + ":t" + currentTerm + ", leader=" + leaderId
+    return getMemberId() + ":t" + currentTerm + ", leader=" + getLeaderId()
         + ", voted=" + votedFor + ", raftlog=" + log + ", conf=" + getRaftConf();
   }
 
