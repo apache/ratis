f6c886a22d9370db70f05adfaab7f9b39617d587
dragonyliu <dragonyliu@tencent.com>
2023-01-12 21:04:15 +0800

    Fix stepDown command don't work issue

--- ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
@@ -1177,7 +1177,8 @@ class RaftServerImpl implements RaftServer.Division,
     assertGroup(request.getRequestorId(), request.getRaftGroupId());
 
     return role.getLeaderState().map(leader -> leader.submitStepDownRequestAsync(request))
-        .orElseGet(() -> CompletableFuture.completedFuture(newSuccessReply(request)));
+        .orElseGet(() -> CompletableFuture.completedFuture(newExceptionReply(
+            request, new NotLeaderException(getMemberId(), getRaftConf().getPeer(info.getLeaderId()), null))));
   }
 
   public RaftClientReply setConfiguration(SetConfigurationRequest request) throws IOException {
