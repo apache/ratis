78386ac77bab42be5dc3f3de50b7f299c59c8465
qianlei <qianlei666@gmail.com>
2023-03-30 16:40:39 +0800

    fix: change catchUp to caughtUp.

--- ratis-server/src/main/java/org/apache/ratis/server/impl/LeaderStateImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/LeaderStateImpl.java
@@ -429,7 +429,7 @@ class LeaderStateImpl implements LeaderState {
 
   @Override
   public boolean onFollowerTerm(FollowerInfo follower, long followerTerm) {
-    if (isCatchUp(follower) && followerTerm > getCurrentTerm()) {
+    if (isCaughtUp(follower) && followerTerm > getCurrentTerm()) {
       submitStepDownEvent(followerTerm, StepDownReason.HIGHER_TERM);
       return true;
     }
@@ -561,7 +561,7 @@ class LeaderStateImpl implements LeaderState {
   @Override
   public AppendEntriesRequestProto newAppendEntriesRequestProto(FollowerInfo follower,
       List<LogEntryProto> entries, TermIndex previous, long callId) {
-    final boolean initializing = isCatchUp(follower);
+    final boolean initializing = isCaughtUp(follower);
     final RaftPeerId targetId = follower.getId();
     return ServerProtoUtils.toAppendEntriesRequestProto(server.getMemberId(), targetId, currentTerm, entries,
         ServerImplUtils.effectiveCommitIndex(raftLog.getLastCommittedIndex(), previous, entries.size()),
@@ -581,10 +581,10 @@ class LeaderStateImpl implements LeaderState {
     return server.getRaftConf().getPeer(id, RaftPeerRole.FOLLOWER, RaftPeerRole.LISTENER);
   }
 
-  private Collection<LogAppender> addSenders(Collection<RaftPeer> newPeers, long nextIndex, boolean catchUp) {
+  private Collection<LogAppender> addSenders(Collection<RaftPeer> newPeers, long nextIndex, boolean caughtUp) {
     final Timestamp t = Timestamp.currentTime().addTimeMs(-server.getMaxTimeoutMs());
     final List<LogAppender> newAppenders = newPeers.stream().map(peer -> {
-      final FollowerInfo f = new FollowerInfoImpl(server.getMemberId(), peer, this::getPeer, t, nextIndex, catchUp);
+      final FollowerInfo f = new FollowerInfoImpl(server.getMemberId(), peer, this::getPeer, t, nextIndex, caughtUp);
       followerInfoMap.put(peer.getId(), f);
       raftServerMetrics.addFollower(peer.getId());
       logAppenderMetrics.addFollowerGauges(peer.getId(), f::getNextIndex, f::getMatchIndex, f::getLastRpcTime);
@@ -726,7 +726,7 @@ class LeaderStateImpl implements LeaderState {
    * 3. Otherwise the peer is making progressing. Keep waiting.
    */
   private BootStrapProgress checkProgress(FollowerInfo follower, long committed) {
-    Preconditions.assertTrue(!isCatchUp(follower));
+    Preconditions.assertTrue(!isCaughtUp(follower));
     final Timestamp progressTime = Timestamp.currentTime().addTimeMs(-server.getMaxTimeoutMs());
     final Timestamp timeoutTime = Timestamp.currentTime().addTimeMs(-3L * server.getMaxTimeoutMs());
     if (follower.getLastRpcResponseTime().compareTo(timeoutTime) < 0) {
@@ -744,7 +744,7 @@ class LeaderStateImpl implements LeaderState {
 
   @Override
   public void onFollowerSuccessAppendEntries(FollowerInfo follower) {
-    if (isCatchUp(follower)) {
+    if (isCaughtUp(follower)) {
       submitUpdateCommitEvent();
     } else {
       eventQueue.submit(checkStagingEvent);
@@ -766,7 +766,7 @@ class LeaderStateImpl implements LeaderState {
       // check progress for the new followers
       final EnumSet<BootStrapProgress> reports = getLogAppenders()
           .map(LogAppender::getFollower)
-          .filter(follower -> !isCatchUp(follower))
+          .filter(follower -> !isCaughtUp(follower))
           .map(follower -> checkProgress(follower, commitIndex))
           .collect(Collectors.toCollection(() -> EnumSet.noneOf(BootStrapProgress.class)));
       if (reports.contains(BootStrapProgress.NOPROGRESS)) {
@@ -1184,7 +1184,7 @@ class LeaderStateImpl implements LeaderState {
     void fail(BootStrapProgress progress) {
       final String message = this + ": Fail to set configuration " + newConf + " due to " + progress;
       LOG.debug(message);
-      stopAndRemoveSenders(s -> !isCatchUp(s.getFollower()));
+      stopAndRemoveSenders(s -> !isCaughtUp(s.getFollower()));
 
       stagingState = null;
       // send back failure response to client's request
@@ -1214,8 +1214,8 @@ class LeaderStateImpl implements LeaderState {
     return getLogAppenders().filter(a -> a.getFollowerId().equals(id)).findAny();
   }
 
-  private static boolean isCatchUp(FollowerInfo follower) {
-    return ((FollowerInfoImpl)follower).isCatchUp();
+  private static boolean isCaughtUp(FollowerInfo follower) {
+    return ((FollowerInfoImpl)follower).isCaughtUp();
   }
 
   @Override
