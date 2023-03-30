82cbf0f371655e7fd856622c93fc70d96fe558fb
qianlei <qianlei666@gmail.com>
2023-03-30 15:41:26 +0800

    style: fix checkstyle.

--- ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/peer/AddCommand.java
+++ ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/peer/AddCommand.java
@@ -36,7 +36,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Command for add ratis server.
