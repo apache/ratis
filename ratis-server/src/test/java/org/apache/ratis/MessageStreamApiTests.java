09eabfe667d068006f5f8ee5cc3a305a61a1bb30
sumitagrawl <sumit.jecrc@gmail.com>
2023-06-01 08:05:36 +0530

    RATIS-1836. Frequent retry by leader on failure path for appendEntry

--- ratis-server/src/test/java/org/apache/ratis/MessageStreamApiTests.java
+++ ratis-server/src/test/java/org/apache/ratis/MessageStreamApiTests.java
@@ -25,7 +25,6 @@ import org.apache.ratis.conf.RaftProperties;
 import org.apache.ratis.protocol.Message;
 import org.apache.ratis.protocol.RaftClientReply;
 import org.apache.ratis.server.RaftServer;
-import org.apache.ratis.server.RaftServerConfigKeys;
 import org.apache.ratis.server.impl.MiniRaftCluster;
 import org.apache.ratis.statemachine.impl.SimpleStateMachine4Testing;
 import org.apache.ratis.statemachine.StateMachine;
