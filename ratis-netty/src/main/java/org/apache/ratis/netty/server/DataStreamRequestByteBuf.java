b49a79317fbc1fcb68448bd603df6f6bf9d18808
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2023-06-19 23:33:46 +0800

    RATIS-1847. Stream has memory leak. (#884)

--- ratis-netty/src/main/java/org/apache/ratis/netty/server/DataStreamRequestByteBuf.java
+++ ratis-netty/src/main/java/org/apache/ratis/netty/server/DataStreamRequestByteBuf.java
@@ -30,6 +30,8 @@ import org.apache.ratis.thirdparty.io.netty.buffer.Unpooled;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Implements {@link DataStreamRequest} with {@link ByteBuf}.
@@ -37,13 +39,13 @@ import java.util.List;
  * This class is immutable.
  */
 public class DataStreamRequestByteBuf extends DataStreamPacketImpl implements DataStreamRequest {
-  private final ByteBuf buf;
+  private final AtomicReference<ByteBuf> buf;
   private final List<WriteOption> options;
 
   public DataStreamRequestByteBuf(ClientId clientId, Type type, long streamId, long streamOffset,
                                   Iterable<WriteOption> options, ByteBuf buf) {
     super(clientId, type, streamId, streamOffset);
-    this.buf = buf != null? buf.asReadOnly(): Unpooled.EMPTY_BUFFER;
+    this.buf = new AtomicReference<>(buf != null? buf.asReadOnly(): Unpooled.EMPTY_BUFFER);
     this.options = Collections.unmodifiableList(Lists.newArrayList(options));
   }
 
@@ -52,13 +54,25 @@ public class DataStreamRequestByteBuf extends DataStreamPacketImpl implements Da
          header.getWriteOptionList(), buf);
   }
 
+  ByteBuf getBuf() {
+    return Optional.ofNullable(buf.get()).orElseThrow(
+        () -> new IllegalStateException("buf is already released in " + this));
+  }
+
   @Override
   public long getDataLength() {
-    return buf.readableBytes();
+    return getBuf().readableBytes();
   }
 
   public ByteBuf slice() {
-    return buf.slice();
+    return getBuf().slice();
+  }
+
+  public void release() {
+    final ByteBuf got = buf.getAndSet(null);
+    if (got != null) {
+      got.release();
+    }
   }
 
   @Override
