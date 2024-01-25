608c8a7e27489d801108a5f4ab6488592b94418c
Duong Nguyen <duongnt.is@gmail.com>
2024-01-24 23:03:49 -0800

    checkstyle.

--- ratis-common/src/main/java/org/apache/ratis/util/ReferenceCountedLeakDetector.java
+++ ratis-common/src/main/java/org/apache/ratis/util/ReferenceCountedLeakDetector.java
@@ -21,7 +21,6 @@ import org.apache.ratis.util.ReferenceCountedObject.ReferenceCountedObjectImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
