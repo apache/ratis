f40424422b692349b5496ee30e24335c8186093b
Potato <tanxinyu@apache.org>
2024-04-04 00:16:16 +0800

    RATIS-2051. Fix Sonar bugs for Ratis. (#1059)

--- ratis-server/src/main/java/org/apache/ratis/server/impl/LeaderStateImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/LeaderStateImpl.java
@@ -269,7 +269,7 @@ class LeaderStateImpl implements LeaderState {
 
   static class FollowerInfoMap {
     private final Map<RaftPeerId, FollowerInfo> map = new ConcurrentHashMap<>();
-
+    @SuppressWarnings({"squid:S3077"}) // Suppress volatile for generic type
     private volatile CurrentOldFollowerInfos followerInfos;
 
     void put(RaftPeerId id, FollowerInfo info) {
@@ -333,6 +333,7 @@ class LeaderStateImpl implements LeaderState {
   private final RaftServerImpl server;
   private final RaftLog raftLog;
   private final long currentTerm;
+  @SuppressWarnings({"squid:S3077"}) // Suppress volatile for generic type
   private volatile ConfigurationStagingState stagingState;
 
   private final FollowerInfoMap followerInfoMap = new FollowerInfoMap();
