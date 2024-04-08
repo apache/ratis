caa35dd0cfcfe9ff648312a8e9684084d2b67900
hao guo <guohao1@360.cn>
2024-04-09 02:10:22 +0800

    RATIS-1519. When DataStreamManagement#read an exception occurs, remove DataStream (#596)

--- ratis-test/src/test/java/org/apache/ratis/datastream/DataStreamTestUtils.java
+++ ratis-test/src/test/java/org/apache/ratis/datastream/DataStreamTestUtils.java
@@ -149,6 +149,7 @@ public interface DataStreamTestUtils {
     @Override
     public CompletableFuture<DataStream> stream(RaftClientRequest request) {
       final SingleDataStream s = new SingleDataStream(request);
+      LOG.info("XXX {} put {}, {}", this, ClientInvocationId.valueOf(request), s);
       streams.put(ClientInvocationId.valueOf(request), s);
       return CompletableFuture.completedFuture(s);
     }
@@ -177,7 +178,9 @@ public interface DataStreamTestUtils {
     }
 
     SingleDataStream getSingleDataStream(ClientInvocationId invocationId) {
-      return streams.get(invocationId);
+      final SingleDataStream s = streams.get(invocationId);
+      LOG.info("XXX {}: get {} return {}", this, invocationId, s);
+      return s;
     }
 
     Collection<SingleDataStream> getStreams() {
@@ -327,6 +330,8 @@ public interface DataStreamTestUtils {
 
   static void assertHeader(RaftServer server, RaftClientRequest header, int dataSize, boolean stepDownLeader)
       throws Exception {
+    LOG.info("XXX {}: dataSize={}, stepDownLeader={}, header={}",
+        server.getId(), dataSize, stepDownLeader, header);
     // check header
     Assertions.assertEquals(RaftClientRequest.dataStreamRequestType(), header.getType());
 
