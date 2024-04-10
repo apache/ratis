8638a2ffa802de576aae546c22160da3d9339a8f
DaveTeng0 <109315747+DaveTeng0@users.noreply.github.com>
2024-04-10 16:38:57 -0700

    Ratis-2040. Fix RaftPeerId generated by command of "raftMetaConf" to use real PeerId (#1060)

--- ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/local/RaftMetaConfCommand.java
+++ ratis-shell/src/main/java/org/apache/ratis/shell/cli/sh/local/RaftMetaConfCommand.java
@@ -24,6 +24,7 @@ import org.apache.ratis.proto.RaftProtos.LogEntryProto;
 import org.apache.ratis.proto.RaftProtos.RaftConfigurationProto;
 import org.apache.ratis.proto.RaftProtos.RaftPeerProto;
 import org.apache.ratis.proto.RaftProtos.RaftPeerRole;
+import org.apache.ratis.protocol.RaftPeerId;
 import org.apache.ratis.shell.cli.RaftUtils;
 import org.apache.ratis.shell.cli.sh.command.AbstractCommand;
 import org.apache.ratis.shell.cli.sh.command.Context;
@@ -32,11 +33,14 @@ import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Command for generate a new raft-meta.conf file based on original raft-meta.conf and new peers,
@@ -49,6 +53,7 @@ public class RaftMetaConfCommand extends AbstractCommand {
   private static final String RAFT_META_CONF = "raft-meta.conf";
   private static final String NEW_RAFT_META_CONF = "new-raft-meta.conf";
 
+  private static final String SEPARATOR = "\\|";
   /**
    * @param context command context
    */
@@ -69,11 +74,49 @@ public class RaftMetaConfCommand extends AbstractCommand {
       printf("peers or path can't be empty.");
       return -1;
     }
+    Set<String> addresses = new HashSet<>();
+    Set<String> ids = new HashSet<>();
     List<RaftPeerProto> raftPeerProtos = new ArrayList<>();
-    for (String address : peersStr.split(",")) {
-      String peerId = RaftUtils.getPeerId(parseInetSocketAddress(address)).toString();
+    for (String idWithAddress : peersStr.split(",")) {
+      String[] peerIdWithAddressArray = idWithAddress.split(SEPARATOR);
+
+      if (peerIdWithAddressArray.length < 1 || peerIdWithAddressArray.length > 2) {
+        String message =
+            "Failed to parse peer's ID and address for: %s, " +
+                "from option: -peers %s. \n" +
+                "Please make sure to provide list of peers" +
+                " in format <[P0_ID|]P0_HOST:P0_PORT,[P1_ID|]P1_HOST:P1_PORT,[P2_ID|]P2_HOST:P2_PORT>";
+        printf(message, idWithAddress, peersStr);
+        return -1;
+      }
+      InetSocketAddress inetSocketAddress = parseInetSocketAddress(
+          peerIdWithAddressArray[peerIdWithAddressArray.length - 1]);
+      String addressString = inetSocketAddress.toString();
+      if (addresses.contains(addressString)) {
+        printf("Found duplicated address: %s. Please make sure the address of peer have no duplicated value.",
+            addressString);
+        return -1;
+      }
+      addresses.add(addressString);
+
+      String peerId;
+      if (peerIdWithAddressArray.length == 2) {
+        // Peer ID is provided
+        peerId = RaftPeerId.getRaftPeerId(peerIdWithAddressArray[0]).toString();
+
+        if (ids.contains(peerId)) {
+          printf("Found duplicated ID: %s. Please make sure the ID of peer have no duplicated value.", peerId);
+          return -1;
+        }
+        ids.add(peerId);
+      } else {
+        // If peer ID is not provided, use host address as peerId value
+        peerId = RaftUtils.getPeerId(inetSocketAddress).toString();
+      }
+
       raftPeerProtos.add(RaftPeerProto.newBuilder()
-          .setId(ByteString.copyFrom(peerId.getBytes(StandardCharsets.UTF_8))).setAddress(address)
+          .setId(ByteString.copyFrom(peerId.getBytes(StandardCharsets.UTF_8)))
+          .setAddress(addressString)
           .setStartupRole(RaftPeerRole.FOLLOWER).build());
     }
     try (InputStream in = Files.newInputStream(Paths.get(path, RAFT_META_CONF));
@@ -93,7 +136,7 @@ public class RaftMetaConfCommand extends AbstractCommand {
   @Override
   public String getUsage() {
     return String.format("%s"
-            + " -%s <PEER0_HOST:PEER0_PORT,PEER1_HOST:PEER1_PORT,PEER2_HOST:PEER2_PORT>"
+            + " -%s <[P0_ID|]P0_HOST:P0_PORT,[P1_ID|]P1_HOST:P1_PORT,[P2_ID|]P2_HOST:P2_PORT>"
             + " -%s <PARENT_PATH_OF_RAFT_META_CONF>",
         getCommandName(), PEER_OPTION_NAME, PATH_OPTION_NAME);
   }
