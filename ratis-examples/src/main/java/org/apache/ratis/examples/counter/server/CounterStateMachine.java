de2e777e19d48bd67fcba1538007940abde87170
Angerszhuuuu <angers.zhu@gmail.com>
2024-06-13 17:15:56 +0800

    RATIS-2111 Reinitialize should load the latest snapshot (#1111)

--- ratis-examples/src/main/java/org/apache/ratis/examples/counter/server/CounterStateMachine.java
+++ ratis-examples/src/main/java/org/apache/ratis/examples/counter/server/CounterStateMachine.java
@@ -138,7 +138,7 @@ public class CounterStateMachine extends BaseStateMachine {
    */
   @Override
   public void reinitialize() throws IOException {
-    load(storage.getLatestSnapshot());
+    load(storage.loadLatestSnapshot());
   }
 
   /**
