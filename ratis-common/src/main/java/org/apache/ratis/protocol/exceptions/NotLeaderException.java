443436455226a70d43a25b61292023e408a1c261
Ming Wei <292619280@qq.com>
2024-07-30 11:52:23 +0800

    RATIS-2126. Optimize NotLeaderException message (#1124)

--- ratis-common/src/main/java/org/apache/ratis/protocol/exceptions/NotLeaderException.java
+++ ratis-common/src/main/java/org/apache/ratis/protocol/exceptions/NotLeaderException.java
@@ -30,7 +30,8 @@ public class NotLeaderException extends RaftException {
   private final Collection<RaftPeer> peers;
 
   public NotLeaderException(RaftGroupMemberId memberId, RaftPeer suggestedLeader, Collection<RaftPeer> peers) {
-    super("Server " + memberId + " is not the leader" + (suggestedLeader != null? " " + suggestedLeader: ""));
+    super("Server " + memberId + " is not the leader" +
+            (suggestedLeader != null ? ", suggested leader is: " + suggestedLeader : ""));
     this.suggestedLeader = suggestedLeader;
     this.peers = peers != null? Collections.unmodifiableCollection(peers): Collections.emptyList();
     Preconditions.assertUnique(this.peers);
