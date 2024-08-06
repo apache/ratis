f09e5af32065b5456d5e063c3dd23a1698298749
Chung En Lee <wfps1210@gmail.com>
2024-08-06 22:01:31 +0800

    RATIS-2134.  `logMetadata` could miss appending the `metadataEntry` with the lastCommitIndex (#1130)
    (cherry picked from commit 4ba463789df7876837c728ce6477fd18b787fed9)

--- ratis-server/src/main/java/org/apache/ratis/server/raftlog/RaftLogBase.java
+++ ratis-server/src/main/java/org/apache/ratis/server/raftlog/RaftLogBase.java
@@ -244,21 +244,9 @@ public abstract class RaftLogBase implements RaftLog {
       // do not log the first conf entry
       return false;
     }
-    ReferenceCountedObject<LogEntryProto> ref = null;
-    try {
-      ref = retainLog(newCommitIndex);
-      if (ref.get().hasMetadataEntry()) {
-        // do not log the metadata entry
-        return false;
-      }
-    } catch(RaftLogIOException e) {
-      LOG.error("Failed to get log entry for index " + newCommitIndex, e);
-    } finally {
-      if (ref != null) {
-        ref.release();
-      }
-    }
-    return true;
+    final LogEntryProto last = lastMetadataEntry.get();
+    // do not log entries with a smaller commit index.
+    return last == null || newCommitIndex > last.getMetadataEntry().getCommitIndex();
   }
 
   @Override
