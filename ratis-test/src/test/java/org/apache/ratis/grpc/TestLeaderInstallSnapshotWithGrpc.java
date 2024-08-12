295ca145fd81899bab009c6d82652c18590c22f6
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2024-08-12 09:21:29 -0700

    RATIS-2115. Fix flaky RetryCacheTests and InstallSnapshotFromLeaderTests. (#1129)

--- ratis-test/src/test/java/org/apache/ratis/grpc/TestLeaderInstallSnapshotWithGrpc.java
+++ ratis-test/src/test/java/org/apache/ratis/grpc/TestLeaderInstallSnapshotWithGrpc.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -24,7 +24,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class TestLeaderInstallSnapshot
+public class TestLeaderInstallSnapshotWithGrpc
 extends InstallSnapshotFromLeaderTests<MiniRaftClusterWithGrpc>
 implements MiniRaftClusterWithGrpc.FactoryGet {
 
