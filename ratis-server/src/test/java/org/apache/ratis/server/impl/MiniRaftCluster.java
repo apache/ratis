2c6efff1f6621d8d1346cf7b35ca62cb26ecb55b
dengchao <2326884052@qq.com>
2024-10-11 11:39:14 +0800

    rollback MiniRaftCluster.start

--- ratis-server/src/test/java/org/apache/ratis/server/impl/MiniRaftCluster.java
+++ ratis-server/src/test/java/org/apache/ratis/server/impl/MiniRaftCluster.java
@@ -346,6 +346,7 @@ public abstract class MiniRaftCluster implements Closeable {
     LOG.info(".............................................................. ");
 
     initServers();
+
     startServers(servers.values());
     this.timer.updateAndGet(t -> t != null? t
         : JavaUtils.runRepeatedly(() -> LOG.info("TIMED-PRINT: " + printServers()), 10, 10, TimeUnit.SECONDS));
