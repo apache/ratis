24882c34321effa498b1c7936953ecda63e96cf3
szywilliam <szywilliam@163.com>
2025-01-10 10:05:09 +0800

    fix cherry-pick generated warnings

--- ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
@@ -118,7 +118,6 @@ import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
