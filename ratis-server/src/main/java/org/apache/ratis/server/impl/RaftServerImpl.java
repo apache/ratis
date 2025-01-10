68ff863e5959c96c3cf71934ea4cd91ff409463c
OneSizeFitQuorum <tanxinyu@apache.org>
2025-01-10 09:35:02 +0800

    finish
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>

--- ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/RaftServerImpl.java
@@ -206,6 +206,14 @@ class RaftServerImpl implements RaftServer.Division,
           .map(LeaderStateImpl::getFollowerNextIndices)
           .orElse(null);
     }
+
+    @Override
+    public long[] getFollowerMatchIndices() {
+      return role.getLeaderState()
+          .filter(leader -> isLeader())
+          .map(LeaderStateImpl::getFollowerMatchIndices)
+          .orElse(null);
+    }
   }
 
   private final RaftServerProxy proxy;
