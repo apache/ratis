58d5e4cfe2f5f1215538cab47525c0a79a58b615
jianghuazhu <740087514@qq.com>
2025-05-03 06:45:59 +0800

    RATIS-2286. Generate the necessary unified names for the submodules under RaftServerImpl (#1251)

--- ratis-server/src/main/java/org/apache/ratis/server/impl/LeaderStateImpl.java
+++ ratis-server/src/main/java/org/apache/ratis/server/impl/LeaderStateImpl.java
@@ -49,6 +49,7 @@ import org.apache.ratis.server.protocol.TermIndex;
 import org.apache.ratis.server.raftlog.LogEntryHeader;
 import org.apache.ratis.server.raftlog.LogProtoUtils;
 import org.apache.ratis.server.raftlog.RaftLog;
+import org.apache.ratis.server.util.ServerStringUtils;
 import org.apache.ratis.statemachine.TransactionContext;
 import org.apache.ratis.util.CodeInjectionForTesting;
 import org.apache.ratis.util.CollectionUtils;
@@ -146,7 +147,7 @@ class LeaderStateImpl implements LeaderState {
   }
 
   private class EventQueue {
-    private final String name = server.getMemberId() + "-" + JavaUtils.getClassSimpleName(getClass());
+    private final String name = ServerStringUtils.generateUnifiedName(server.getMemberId(), getClass());
     private final BlockingQueue<StateUpdateEvent> queue = new ArrayBlockingQueue<>(4096);
 
     void submit(StateUpdateEvent event) {
@@ -360,7 +361,7 @@ class LeaderStateImpl implements LeaderState {
   private final LeaderLease lease;
 
   LeaderStateImpl(RaftServerImpl server) {
-    this.name = server.getMemberId() + "-" + JavaUtils.getClassSimpleName(getClass());
+    this.name = ServerStringUtils.generateUnifiedName(server.getMemberId(), getClass());
     this.server = server;
 
     final RaftProperties properties = server.getRaftServer().getProperties();
@@ -1234,7 +1235,7 @@ class LeaderStateImpl implements LeaderState {
   }
 
   private class ConfigurationStagingState {
-    private final String name = server.getMemberId() + "-" + JavaUtils.getClassSimpleName(getClass());
+    private final String name = ServerStringUtils.generateUnifiedName(server.getMemberId(), getClass());
     private final Map<RaftPeerId, RaftPeer> newPeers;
     private final Map<RaftPeerId, RaftPeer> newListeners;
     private final PeerConfiguration newConf;
