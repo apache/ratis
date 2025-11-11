9052dadbb1c384155414eed171b541b83dae5c5b
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2025-11-11 07:41:34 -0800

    RATIS-2353. Refactor ReadOnlyRequestTests. (#1308)

---
+++ ratis-test/src/test/java/org/apache/ratis/grpc/TestLinearizableLeaderLeaseReadWithGrpc.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.ratis.grpc;
+
+import org.apache.ratis.LinearizableReadTests;
+import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.server.RaftServerConfigKeys;
+
+import static org.apache.ratis.ReadOnlyRequestTests.assertOption;
+import static org.apache.ratis.server.RaftServerConfigKeys.Read.Option.LINEARIZABLE;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestLinearizableLeaderLeaseReadWithGrpc
+  extends LinearizableReadTests<MiniRaftClusterWithGrpc>
+  implements MiniRaftClusterWithGrpc.FactoryGet {
+
+  @Override
+  public boolean isLeaderLeaseEnabled() {
+    return true;
+  }
+
+  @Override
+  public void assertRaftProperties(RaftProperties p) {
+    assertOption(LINEARIZABLE, p);
+    assertTrue(RaftServerConfigKeys.Read.leaderLeaseEnabled(p));
+    assertTrue(isLeaderLeaseEnabled());
+  }
+}
