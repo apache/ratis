58431c07e76065a6da76ac8d0e725b7f18d839e2
Ivan Andika <ivandika@apache.org>
2026-01-21 02:27:05 +0800

    RATIS-2383. AtomicFileOutputStream should override FilterOutputStream batch write API (#1336)

--- ratis-common/src/main/java/org/apache/ratis/util/AtomicFileOutputStream.java
+++ ratis-common/src/main/java/org/apache/ratis/util/AtomicFileOutputStream.java
@@ -68,6 +68,11 @@ public class AtomicFileOutputStream extends FilterOutputStream {
     this.tmpFile = tmpFile.getAbsoluteFile();
   }
 
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException {
+    out.write(b, off, len);
+  }
+
   public boolean isClosed() {
     return isClosed.get();
   }
