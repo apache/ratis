357cb71e7a4c379d78ecfae194612c6087a05160
peterxcli <peterxcli@gmail.com>
2026-06-13 21:56:01 +0800

    stream client handling
    Signed-off-by: peterxcli <peterxcli@gmail.com>

--- ratis-test/src/test/java/org/apache/ratis/client/impl/TestDataStreamClientImpl.java
+++ ratis-test/src/test/java/org/apache/ratis/client/impl/TestDataStreamClientImpl.java
@@ -19,27 +19,22 @@ package org.apache.ratis.client.impl;
 
 import org.apache.ratis.client.DataStreamClient;
 import org.apache.ratis.client.DataStreamClientRpc;
-import org.apache.ratis.client.RaftClient;
-import org.apache.ratis.client.RaftClientRpc;
 import org.apache.ratis.client.api.DataStreamInput;
 import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.datastream.impl.DataStreamReplyByteBuffer;
 import org.apache.ratis.datastream.impl.DataStreamRequestByteBuffer;
+import org.apache.ratis.proto.RaftProtos.DataStreamPacketHeaderProto.Type;
 import org.apache.ratis.proto.RaftProtos.RaftClientRequestProto;
 import org.apache.ratis.protocol.ClientId;
 import org.apache.ratis.protocol.DataStreamReply;
 import org.apache.ratis.protocol.DataStreamRequest;
-import org.apache.ratis.protocol.RaftClientReply;
 import org.apache.ratis.protocol.RaftClientRequest;
-import org.apache.ratis.protocol.RaftGroup;
 import org.apache.ratis.protocol.RaftGroupId;
 import org.apache.ratis.protocol.RaftPeer;
-import org.apache.ratis.retry.RetryPolicies;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
@@ -49,23 +44,9 @@ public class TestDataStreamClientImpl {
     return RaftPeer.newBuilder().setId(id).build();
   }
 
-  private static class NoOpRaftClientRpc implements RaftClientRpc {
-    @Override
-    public void addRaftPeers(Collection<RaftPeer> peers) {
-    }
-
-    @Override
-    public RaftClientReply sendRequest(RaftClientRequest request) throws IOException {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void close() {
-    }
-  }
-
   private static class RecordingDataStreamClientRpc implements DataStreamClientRpc {
     private final AtomicReference<RaftClientRequest> request = new AtomicReference<>();
+    private final AtomicReference<Consumer<DataStreamReply>> replyConsumer = new AtomicReference<>();
 
     @Override
     public CompletableFuture<DataStreamReply> streamAsync(
@@ -76,6 +57,7 @@ public class TestDataStreamClientImpl {
       } catch (Exception e) {
         throw new IllegalStateException(e);
       }
+      this.replyConsumer.set(replyConsumer);
       return new CompletableFuture<>();
     }
 
@@ -83,36 +65,58 @@ public class TestDataStreamClientImpl {
       return request.get();
     }
 
+    void receive(DataStreamReply reply) {
+      replyConsumer.get().accept(reply);
+    }
+
     @Override
     public void close() {
     }
   }
 
+  private static DataStreamClient newDataStreamClient(
+      RaftPeer dataStreamServer, RecordingDataStreamClientRpc dataStreamClientRpc) {
+    final RaftProperties properties = new RaftProperties();
+    return new DataStreamClientImpl(
+        ClientId.randomId(), RaftGroupId.randomId(), dataStreamServer, dataStreamClientRpc, properties);
+  }
+
   @Test
   public void testStreamReadOnlyUsesPrimaryDataStreamServer() throws Exception {
-    final RaftProperties properties = new RaftProperties();
-    final RaftPeer leader = newPeer("leader");
     final RaftPeer follower = newPeer("follower");
-    final RaftGroup group = RaftGroup.valueOf(RaftGroupId.randomId(), leader, follower);
     final RecordingDataStreamClientRpc dataStreamClientRpc = new RecordingDataStreamClientRpc();
 
-    try (RaftClient client = RaftClient.newBuilder()
-        .setClientId(ClientId.randomId())
-        .setRaftGroup(group)
-        .setLeaderId(leader.getId())
-        .setPrimaryDataStreamServer(follower)
-        .setClientRpc(new NoOpRaftClientRpc())
-        .setProperties(properties)
-        .setRetryPolicy(RetryPolicies.noRetry())
-        .build();
-         DataStreamClient dataStreamClient = DataStreamClient.newBuilder(client)
-             .setDataStreamServer(follower)
-             .setDataStreamClientRpc(dataStreamClientRpc)
-             .setProperties(properties)
-             .build();
+    try (DataStreamClient dataStreamClient = newDataStreamClient(follower, dataStreamClientRpc);
          DataStreamInput input = dataStreamClient.streamReadOnly(ByteBuffer.wrap(new byte[] {1}))) {
       Assertions.assertNotNull(input);
       Assertions.assertEquals(follower.getId(), dataStreamClientRpc.getRequest().getServerId());
+    }
+  }
+
+  @Test
+  public void testReceiveSkipsCancelledPendingRead() throws Exception {
+    final RaftPeer follower = newPeer("follower");
+    final RecordingDataStreamClientRpc dataStreamClientRpc = new RecordingDataStreamClientRpc();
+
+    try (DataStreamClient dataStreamClient = newDataStreamClient(follower, dataStreamClientRpc);
+         DataStreamInput input = dataStreamClient.streamReadOnly(ByteBuffer.wrap(new byte[] {1}))) {
+      final CompletableFuture<DataStreamReply> cancelled = input.readAsync();
+      final CompletableFuture<DataStreamReply> active = input.readAsync();
+      cancelled.cancel(false);
+
+      final DataStreamReply reply = DataStreamReplyByteBuffer.newBuilder()
+          .setClientId(ClientId.randomId())
+          .setType(Type.STREAM_DATA)
+          .setStreamId(1)
+          .setStreamOffset(0)
+          .setBuffer(ByteBuffer.allocate(0))
+          .setSuccess(true)
+          .build();
+      dataStreamClientRpc.receive(reply);
+
+      Assertions.assertTrue(active.isDone());
+      Assertions.assertSame(reply, active.getNow(null));
+      reply.release();
     }
   }
 }
