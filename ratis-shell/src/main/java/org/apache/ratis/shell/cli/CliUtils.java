37932bf60111bd1a26d047d9bd33c46faa5f7022
Abhishek Pal <pal.abhishek03012001@gmail.com>
2026-07-21 01:25:36 +0530

    RATIS-2610. -groupid should check for non-empty ID (#1523)

--- ratis-shell/src/main/java/org/apache/ratis/shell/cli/CliUtils.java
+++ ratis-shell/src/main/java/org/apache/ratis/shell/cli/CliUtils.java
@@ -96,7 +96,7 @@ public final class CliUtils {
 
   /** Parse the given string as a {@link RaftGroupId}. */
   public static RaftGroupId parseRaftGroupId(String groupId) {
-    return groupId != null && groupId.isEmpty() ? RaftGroupId.valueOf(UUID.fromString(groupId)) : null;
+    return groupId != null && !groupId.isEmpty() ? RaftGroupId.valueOf(UUID.fromString(groupId)) : null;
   }
 
   /**
