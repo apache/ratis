7f8dded6540b86fde1902b60eab9fd543ad08e58
szetszwo <szetszwo@apache.org>
2026-09-18 22:52:03 +0800

    RATIS-2705. Improve Division leadership API

--- ratis-server/src/test/java/org/apache/ratis/RaftExceptionBaseTest.java
+++ ratis-server/src/test/java/org/apache/ratis/RaftExceptionBaseTest.java
@@ -101,14 +101,16 @@ public abstract class RaftExceptionBaseTest<CLUSTER extends MiniRaftCluster>
   public void testNewNotLeaderException() throws Exception {
     runWithNewCluster(NUM_PEERS, cluster -> {
       final RaftServer.Division leader = RaftTestUtil.waitForLeader(cluster);
-      final NotLeaderException leaderException = leader.newNotLeaderException();
+      final NotLeaderException leaderException = (NotLeaderException) leader.newLeadershipException(
+          RaftServer.LeadershipStatus.NOT_LEADER);
       Assertions.assertNull(leaderException.getSuggestedLeader());
       Assertions.assertEquals(cluster.getPeers().size(), leaderException.getPeers().size());
       Assertions.assertTrue(cluster.getPeers().containsAll(leaderException.getPeers()));
 
       final RaftServer.Division follower = cluster.getFollowers().get(0);
       follower.close();
-      final NotLeaderException closedException = follower.newNotLeaderException();
+      final NotLeaderException closedException = (NotLeaderException) follower.newLeadershipException(
+          RaftServer.LeadershipStatus.NOT_LEADER);
       Assertions.assertNull(closedException.getSuggestedLeader());
       Assertions.assertTrue(closedException.getPeers().isEmpty());
     });
